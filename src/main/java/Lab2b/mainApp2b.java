5ac459cc2d356e4785cebc1db26ffaf85d1c9c5c
William Tseng <wytseng@connect.ust.hk>
2023-09-20 21:25:27 +0800

    Add readme.md and screenshot to /Lab1

---
+++ src/main/java/Lab2b/mainApp2b.java
@@ -0,0 +1,13 @@
+package Lab2b;
+
+public class mainApp2b {
+    public static void main(String arg[]){
+        final String array[] = {"Basic Java","Advanced Java","Guru Java"};
+        Book b = new Book(array);
+        int k=2;
+        System.out.println("The title of Chapter "+k+" is "+b.getChapter((k-1)));
+        String anotherArray[] = b.getChapters();
+        System.out.println("There are "+anotherArray.length+" cahpters.");
+        System.out.println(anotherArray);
+    }
+}
